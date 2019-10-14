82c4db05e895b7f401b5ecca98da7a5552b98f19
soichi-fujiwara <52342154+soichi-fujiwara@users.noreply.github.com>
2019-10-14 16:53:16 +0900

    Update main.py

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ def index():
   if request.method == 'POST':
 
     rev_word = ""
-    words = request.form['name'].decode(encoding='utf-8')
+    words = request.form['name']
     name = request.form['name']
 
     from google.cloud import storage as gcs
@@ -47,13 +47,10 @@ def index():
 
     #Blobを作成
     blob = gcs.Blob(fname, bucket)
-    #content = blob.download_as_string()
-
     #★modelがbytesオブジェクトだと後続でエラーとなる
-    model = blob.download_as_string()
+    model = blob.download_as_string().decode()
 
     #MAIN
-    #Bytes型だとエラーになる為str型に変換
     words = words[0:16]
     gyaku = u"逆"
     inherent_words = '[' + words + ']'
