22a231b4416157945ec2326df34252ae2ea2a367
soichi-fujiwara <52342154+soichi-fujiwara@users.noreply.github.com>
2019-10-30 15:21:39 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template, request, redirect, url_for
 
 #対義語生成
 #import lib_wordRevChange as lw
-import lib_antnym as ant
+import lib_antnym_gae as ant
 
 
 app = Flask(__name__)
