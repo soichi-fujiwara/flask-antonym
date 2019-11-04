71a526280cc30784a65f412d35724ba81f7b6ed3
soichi-fujiwara <52342154+soichi-fujiwara@users.noreply.github.com>
2019-11-04 22:56:30 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from flask import Flask, render_template, request, redirect, url_for
+import dask.dataframe as dd
 
 #対義語生成
 #import lib_wordRevChange as lw
@@ -19,7 +20,7 @@ def index():
 
     #対義語取得
     words = request.form['words'][0:12]
-    ant_word_list = ant.get_ant_word(words)
+    ant_word_list = ant.get_ant_word(words,df_ant)
     return render_template('index.html',in_words=words,ant_word_list=ant_word_list)
   else:
     #初期表示
@@ -28,4 +29,6 @@ def index():
 
 if __name__ == '__main__':
     app.debug = True
+    csv_dir = 'csv/df_ant_words.csv'
+    df_ant = dd.read_csv(csv_dir,header=0, names=('words','ant1','ant2','ant3','flg')).compute()
     app.run(host='0.0.0.0')
