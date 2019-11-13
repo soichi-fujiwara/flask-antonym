202fd82714f7368804dc090de71bc80dfaa0c1ea
soichi-fujiwara <52342154+soichi-fujiwara@users.noreply.github.com>
2019-11-13 22:44:21 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from flask import Flask, render_template, request, redirect, url_for
-
+import random
 
 #対義語生成
 #import lib_wordRevChange as lw
@@ -15,17 +15,34 @@ app = Flask(__name__)
 def index():
 
   ant_word_list = []
-
   if request.method == 'POST':
+
+  #参考値作成
+  auto_comp_list = ['(例)好きなジブリ映画',
+                    '(例)好きな観光地',
+                    '(例)好きな小説',
+                    '(例)好きなドラマ',
+                    '(例)座右の銘',
+                    '(例)好きな店の名前,
+                    '(例)気になる学校の名前',
+                    '(例)キャラクターの名前',
+                    '(例)気になる職業の名前',
+                    '(例)好きな曲名',
+                    '(例)好きなお菓子',
+                    '(例)好きなフレーズ',
+                    '(例)想い出の場所']
+
+    auto_comp = auto_comp_list[random.randint(0,12)]
 
     #対義語取得
     words = request.form['words'][0:12]
     ant_word_list = ant.get_ant_word(words)
-    return render_template('index.html',in_words=words,ant_word_list=ant_word_list)
+    return render_template('index.html',auto_comp=auto_comp,in_words=words,ant_word_list=ant_word_list)
   else:
     #初期表示
     words = ""
-    return render_template('index.html',in_words=words,ant_word_list=ant_word_list)
+    auto_comp = "単語や文章を入力して下さい"
+    return render_template('index.html',auto_comp=auto_comp,in_words=words,ant_word_list=ant_word_list)
 
 if __name__ == '__main__':
     app.debug = True
