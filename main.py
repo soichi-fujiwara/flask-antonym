34d55a19d967e2a293b905c1a0b4009baabd6109
soichi-fujiwara <52342154+soichi-fujiwara@users.noreply.github.com>
2019-12-08 06:42:53 +0900

    Update main.py

--- main.py
+++ main.py
@@ -20,8 +20,27 @@ def index():
   if request.method == 'POST':
 
     #対義語取得
+    #1回目
     words = request.form['words'][0:12].replace(' ','').replace('　','')
     ant_word_list = ant.get_ant_word(words)
+    
+    #2回目
+    words_2 = words
+    tokenizer = MeCab.Tagger("")
+    if ant_word_list == ['該当なし']:
+      for num in range(2,len(words)-2):
+        node = tokenizer.parse(words[num:]).split("\n")
+        for nd in node:
+          #表層形(IN)=発音が一致する → 辞書に単語登録がある可能性が高い性質を利用
+          # 表層形(IN) : words[num:]
+          # 発音       : nd.split(',')[-1] (最終項目)
+          if words[num:] == nd.split(',')[-1]:
+            words_2 = words[0:num] + '@' + words[num:]
+            break
+
+      if words_2 != words: 
+        ant_word_list = replace(ant.get_ant_word(words),'@','')
+      
     return render_template('index.html',auto_comp=auto_comp,in_words=words,ant_word_list=ant_word_list)
   else:
     #初期表示
